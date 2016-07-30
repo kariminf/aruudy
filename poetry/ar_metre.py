17878cc0cfac325afaf76e1a5f9535514904b881
Abdelkrime Aries <kariminfo0@gmail.com>
2016-07-30 11:59:19 +0100

    fix al- and alif-tanween pblms

--- poetry/ar_metre.py
+++ poetry/ar_metre.py
@@ -23,8 +23,8 @@
 
 import re
 
-# x = [u-]
-# 
+sun = u"[\u062A\u062B\u062F\u0630\u0631\u0632\u0633\u0634\u0635\u0636\u0637\u0638\u0644\u0646]"
+
 metres =	{
     "u-[u-]u-[u-]-u-[u-]u-u-": "tawiil",
     "[u-]u-[u-]u-[u-]u-": "madiid",
@@ -44,18 +44,33 @@ metres =	{
     "u-[u-]u-[u-]u-[u-]u-": "mutaqaarib"
 }
 
+def ar_filter(text):
+	nwtext = text
+	
+	#delete tatweel
+	nwtext = re.sub(ur'\u0640', '', nwtext)
+	
+	#delete a lot of chars
+	#nwtext = re.sub(ur'[^\u0621-\u063A\u0641-\u0652]', '', nwtext)
+	
+	return nwtext
+	
 def fix_al(text):
 	nwtext = text
-	#Replace al- with C if preceded by fatha, damma or kasra followed by space
-	nwtext = re.sub(ur'([^\s]\s+)\u0627\u0644', ur'\1\u0644', nwtext)
-	#Replace al- with VC if it is in the first line
+	#al- in the first sentence: add fatha
 	nwtext = re.sub(ur'^\s*\u0627\u0644', ur'\u0627\u064E\u0644', nwtext)
+	
+	#Replace al- with sun character (it can be preceded by prepositions bi- li-)
+	nwtext = re.sub(ur'([^\s]\s+|(?:\u0644|\u0628)[\u0650]?)\u0627\u0644(' + sun + ')', ur'\1\2', nwtext)
+	
+	#Replace al- with l otherwise
+	nwtext = re.sub(ur'([^\s]\s+|(?:\u0644|\u0628).?)\u0627\u0644', ur'\1\u0644', nwtext)
 	return nwtext
 	
 def fix_awy(text):
 	nwtext = text
 	#alif
-	nwtext = re.sub(ur'([^\u064E\u064F\u0650\u0652])\u0627([^\u064E\u064F\u0650\u0652])', ur'\1\u064E\u0627\2', nwtext) 
+	nwtext = re.sub(ur'([^\u064E\u064F\u0650\u0652])\u0627([^\u064E\u064F\u0650\u0652])', ur'\g<1>\u064E\u0627\g<2>', nwtext) 
 	
 	#waw
 	nwtext = re.sub(ur'([^\u064E\u064F\u0650\u0652])\u0648([^\u064E\u064F\u0650\u0652])', ur'\1\u064F\u0648\2', nwtext) 
@@ -65,25 +80,40 @@ def fix_awy(text):
 	
 	return nwtext
 	
-def get_cv (ar_word):
-	new_word = ar_word
+def get_cv (text):
+	nwtext = text
 	
-	#Delete sukuun and tatweel and spaces
-	new_word = re.sub(ur'[\u0652\u0640\s]', '', new_word)
+	#Delete sukuun
+	nwtext = re.sub(ur'\u0652', '', nwtext)
 	
-	#Shadda don't need to be replaced because it will be replaced after
+	#Shadda replace (no need, it will be considered as a consonent)
+	#nwtext = re.sub(ur'(.)\u0651', ur'\1\1', nwtext) 
+	
+	#space + lam + consonent + shadda
+	#nwtext = re.sub(ur'\s\u0644[^\u064B\u064C\u064D]\u0651', u'\u0651', nwtext) 
+	
 	#Replace fatha, damma & kasra with (V) for vowel
-	new_word = re.sub(ur'[^CV][\u064E\u064F\u0650]', 'V', new_word) 
+	nwtext = re.sub(ur'[^CV][\u064E\u064F\u0650]', 'V', nwtext) 
+	
+	#delete fathatayn over alif
+	nwtext = re.sub(ur'\u0627\u064B', u'\u0627', nwtext)
 	
 	#Replace fathatayn, dammatayn, kasratayn with Vowel follewed by Consonent
-	new_word = re.sub(ur'[^CV][\u064B\u064C\u064D]', 'VC', new_word)
+	nwtext = re.sub(ur'[^CV][\u064B\u064C\u064D]', 'VC', nwtext)
+	
+	#return nwtext
+	#alif-alifmaqsora + space + consonent => delete madd
+	nwtext = re.sub(ur'[\u0627\u0649]\s+[^V]', 'C', nwtext)
 
+	#Delete space
+	nwtext = re.sub(ur'\s', '', nwtext)
+	
 	#Replace all what is left as it was a consonent
-	new_word = re.sub(ur'[^CV]', 'C', new_word)
+	nwtext = re.sub(ur'[^CV]', 'C', nwtext)
 	
-	new_word = re.sub(ur'V$', 'VC', new_word)
+	nwtext = re.sub(ur'V$', 'VC', nwtext)
 	
-	return new_word
+	return nwtext
 	
 def get_metre (cv):
 	metre = cv
@@ -102,14 +132,16 @@ if __name__ == '__main__':
 	
 	r = u'أَسِرْبَ القَطا هَلْ مَنْ يُعِيْرُ جَناحَهُ'
 	#r = u'الأُمُّ مَـدْرَسَــةٌ إِذَا أَعْـدَدْتَـهَـا'
-	r = u'لَيْـسَ يَرْقَـى الأَبْنَـاءُ فِـي أُمَّـةٍ مَـا'
-	r = u'العَيْـشُ مَاضٍ فَأَكْـرِمْ وَالِدَيْـكَ بِـهِ'
-	r = u'أَحِـنُّ إِلَى الكَـأْسِ التِي شَـرِبَتْ بِهَـا'
-	r = u'أَطِــعِ الإِلَــهَ كَـمَـا أَمَــرْ'
-	r = u'أَعْـطِ أَبَـاكَ النِّصْـفَ حَيًّـا وَمَيِّتـاً'
-	r = u'تَحَمَّـلْ عَـنْ أَبِيْـكَ الثِّقْـلَ يَوْمـاً'
-	r = u'إِذَا كَـانَ رَبُّ البَيْـتِ بِالطَّبْـلِ ضَـارِباً'
+	#r = u'لَيْـسَ يَرْقَـى الأَبْنَـاءُ فِـي أُمَّـةٍ مَـا' #pblm
+	#r = u'العَيْـشُ مَاضٍ فَأَكْـرِمْ وَالِدَيْـكَ بِـهِ'
+	#r = u'أَحِـنُّ إِلَى الكَـأْسِ التِي شَـرِبَتْ بِهَـا' #pblm
+	#r = u'أَطِــعِ الإِلَــهَ كَـمَـا أَمَــرْ' #pblm
+	#r = u'أَعْـطِ أَبَـاكَ النِّصْـفَ حَيًّـا وَمَيِّتـاً' #pblm
+	#r = u'تَحَمَّـلْ عَـنْ أَبِيْـكَ الثِّقْـلَ يَوْمـاً'
+	#r = u'إِذَا كَـانَ رَبُّ البَيْـتِ بِالطَّبْـلِ ضَـارِباً'
 	print(r)
+	
+	r = ar_filter(r)
 	
 	r = fix_al(r)
 	print(r)
