691d3bd16204fd5768a773d60c17492ec9e6a0d3
kariminf <kariminfo0@gmail.com>
2019-01-01 23:07:20 +0100

    web api

---
+++ web/api.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#  Copyright 2019 Abdelkrime Aries <kariminfo0@gmail.com>
+#
+#  ---- AUTHORS ----
+#  2019    Abdelkrime Aries <kariminfo0@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from flask import Flask, jsonify, Response
+
+import sys, os
+reload(sys)
+sys.setdefaultencoding('utf8')
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from aruudy.poetry import bahr
+from aruudy.poetry.bahr import Bahr, BahrError
+
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+#headers = {
+#    "Content-Type": "application/json",0
+#    "charset": "utf-8"
+#}
+
+@app.route('/info/<name>', methods=["GET"])
+def info(name):
+    b = bahr.get_bahr(name)
+    if b == None:
+        return "Bahr not found"
+    return jsonify(b), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- web/cgi.py
+++ web/cgi.py
@@ -3,12 +3,12 @@
 #
 #  test.py
 #  test the aruud on the web
-#  
-#  Copyright 2016 Abdelkrime Aries <kariminfo0@gmail.com>
-#  
+#
+#  Copyright 2016,2019 Abdelkrime Aries <kariminfo0@gmail.com>
+#
 #  ---- AUTHORS ----
-#  2016    Abdelkrime Aries <kariminfo0@gmail.com>
-#  
+#  2016,2019    Abdelkrime Aries <kariminfo0@gmail.com>
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -20,20 +20,18 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#   
+#
 
-# enable debugging
-import cgi
-import cgitb
-import json
-import sys  
-
-reload(sys)  
+import sys, os
+reload(sys)
 sys.setdefaultencoding('utf8')
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+import cgi, cgitb
 import urlparse, urllib
+import json
+from aruudy.poetry import armetre
 
-import ar_metre
 
 cgitb.enable()
 
@@ -41,13 +39,12 @@ args = cgi.FieldStorage()
 
 res = {}
 
-r = u""
 
 r = args.getvalue("text")
 
 r = unicode(r)
 
-r = ar_metre.ar_filter(r)
+r = armetre.ar_filter(r)
 res["filtred"] = r.encode('utf-8')
 
 r = ar_metre.fix_al(r)
@@ -55,16 +52,16 @@ res["fix-al"] = r.encode('utf-8')
 
 r = ar_metre.fix_awy(r)
 res["fix-awy"] = r.encode('utf-8')
-    
+
 r = ar_metre.get_cv(r)
 res["cv"] = r.encode('utf-8')
-    
+
 r = ar_metre.get_metre(r)
 res["metre"] = r.encode('utf-8')
-    
+
 r = ar_metre.get_metre_name(r)
 res["metre-name"] = r
-    
+
 print "Content-type: application/json;charset=utf-8"
 print
 print (json.dumps(res))
